6aa1eb95defba1586fe935e4cf7eb8432de18005
mhotchkiss <mhotchkiss@localhost.localdomain>
2015-04-28 20:06:36 -0600

    Add List Injection

---
+++ Java/Spring/StudyTrails/SpringListInjection/src/com/studytrails/tutorials/springinjectionlist/ToDoList.java
@@ -0,0 +1,17 @@
+package com.studytrails.tutorials.springinjectionlist;
+
+import java.util.List;
+
+public class ToDoList {
+
+	private List<String> tasks;
+
+	public List<String> getTasks() {
+		return tasks;
+	}
+
+	public void setTasks(List<String> tasks) {
+		this.tasks = tasks;
+	}
+
+}
