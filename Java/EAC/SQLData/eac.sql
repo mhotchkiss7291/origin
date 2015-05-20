61a4346f7b08d59eb8923a5bc000a5fe70263505
mhotchkiss <mhotchkiss@localhost.localdomain>
2015-05-20 13:27:17 -0600

    Add sql table add

--- Java/EAC/SQLData/eac.sql
+++ Java/EAC/SQLData/eac.sql
@@ -1,5 +1,5 @@
-
 DROP TABLE IF EXISTS `critic`;
+DROP TABLE IF EXISTS `opinion`;
 
 CREATE TABLE `critic` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -10,5 +10,18 @@ CREATE TABLE `critic` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=41 DEFAULT CHARSET=latin1;
 
+CREATE TABLE `opinion` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `critic` varchar(45) DEFAULT NULL,
+  `description` blob,
+  `expertise` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=41 DEFAULT CHARSET=latin1;
+
+INSERT INTO `opinion` VALUES 
+	(1,'Mark Hotchkiss','I hated this movie','Movies');
+	
 LOCK TABLES `critic` WRITE;
-INSERT INTO `critic` VALUES (1,'Mark Hotchkiss','mark.hotchkiss@gmail.com','Just a guy','Movies'), (2,'Wolf J. Flywheel','groucho@gmail.com','Attorney at Law','Movies');
+INSERT INTO `critic` VALUES 
+	(1,'Mark Hotchkiss','mark.hotchkiss@gmail.com','Just a guy','Movies'), 
+	(2,'Wolf J. Flywheel','groucho@gmail.com','Attorney at Law','Movies');
