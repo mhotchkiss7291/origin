38e204aaeaa2d93c84674dc767412bdc0cf30b52
mhotchkiss <mhotchkiss@localhost.localdomain>
2015-05-28 12:35:10 -0600

    Readd old version

---
+++ Java/EAC/WebContent/js/main.js
@@ -0,0 +1,184 @@
+// The root URL for the RESTful services
+var rootURL = "http://localhost:8080/eac/rest/critic";
+
+var currentCritic;
+
+// Retrieve critic list when application starts
+findAll();
+
+// Nothing to delete in initial application state
+$('#btnDelete').hide();
+
+// Register listeners
+$('#btnSearch').click(function() {
+	search($('#searchKey').val());
+	return false;
+});
+
+// Trigger search when pressing 'Return' on search key input field
+$('#searchKey').keypress(function(e) {
+	if (e.which == 13) {
+		search($('#searchKey').val());
+		e.preventDefault();
+		return false;
+	}
+});
+
+$('#btnAdd').click(function() {
+	newCritic();
+	return false;
+});
+
+$('#btnSave').click(function() {
+	if ($('#criticId').val() == '')
+		addCritic();
+	else
+		updateCritic();
+	return false;
+});
+
+$('#btnDelete').click(function() {
+	deleteCritic();
+	return false;
+});
+
+$('#criticList a').live('click', function() {
+	findById($(this).data('identity'));
+});
+
+// Replace broken images with generic critic bottle
+$("img").error(function() {
+	$(this).attr("src", "pics/generic.jpg");
+
+});
+
+function search(searchKey) {
+	if (searchKey == '')
+		findAll();
+	else
+		findByName(searchKey);
+}
+
+function newCritic() {
+	$('#btnDelete').hide();
+	currentCritic = {};
+	renderDetails(currentCritic); // Display empty form
+}
+
+function findAll() {
+	console.log('findAll');
+	$.ajax({
+		type : 'GET',
+		url : rootURL,
+		dataType : "json", // data type of response
+		success : renderList
+	});
+}
+
+function findByName(searchKey) {
+	console.log('findByName: ' + searchKey);
+	$.ajax({
+		type : 'GET',
+		url : rootURL + '/search/' + searchKey,
+		dataType : "json",
+		success : renderList
+	});
+}
+
+function findById(id) {
+	console.log('findById: ' + id);
+	$.ajax({
+		type : 'GET',
+		url : rootURL + '/' + id,
+		dataType : "json",
+		success : function(data) {
+			$('#btnDelete').show();
+			console.log('findById success: ' + data.name);
+			currentCritic = data;
+			renderDetails(currentCritic);
+		}
+	});
+}
+
+function addCritic() {
+	console.log('addCritic');
+	$.ajax({
+		type : 'POST',
+		contentType : 'application/json',
+		url : rootURL,
+		dataType : "json",
+		data : formToJSON(),
+		success : function(data, textStatus, jqXHR) {
+			alert('Critic created successfully');
+			$('#btnDelete').show();
+			$('#criticId').val(data.id);
+		},
+		error : function(jqXHR, textStatus, errorThrown) {
+			alert('addCritic error: ' + textStatus);
+		}
+	});
+}
+
+function updateCritic() {
+	console.log('updateCritic');
+	$.ajax({
+		type : 'PUT',
+		contentType : 'application/json',
+		url : rootURL + '/' + $('#criticId').val(),
+		dataType : "json",
+		data : formToJSON(),
+		success : function(data, textStatus, jqXHR) {
+			alert('Critic updated successfully');
+		},
+		error : function(jqXHR, textStatus, errorThrown) {
+			alert('updateCritic error: ' + textStatus);
+		}
+	});
+}
+
+function deleteCritic() {
+	console.log('deleteCritic');
+	$.ajax({
+		type : 'DELETE',
+		url : rootURL + '/' + $('#criticId').val(),
+		success : function(data, textStatus, jqXHR) {
+			alert('Critic deleted successfully');
+		},
+		error : function(jqXHR, textStatus, errorThrown) {
+			alert('deleteCritic error');
+		}
+	});
+}
+
+function renderList(data) {
+	// JAX-RS serializes an empty list as null, and a 'collection of one' as an
+	// object (not an 'array of one')
+	var list = data == null ? [] : (data instanceof Array ? data : [ data ]);
+
+	$('#criticList li').remove();
+	$.each(list, function(index, critic) {
+		$('#criticList').append(
+				'<li><a href="#" data-identity="' + critic.id + '">' + critic.name
+						+ '</a></li>');
+	});
+}
+
+function renderDetails(critic) {
+	$('#criticId').val(critic.id);
+	$('#name').val(critic.name);
+	$('#email').val(critic.email);
+	$('#expertise').val(critic.expertise);
+	$('#description').val(critic.description);
+}
+
+// Helper function to serialize all the form fields into a JSON string
+function formToJSON() {
+	var criticId = $('#criticId').val();
+	return JSON.stringify({
+		"id" : criticId == "" ? null : criticId,
+		"name" : $('#name').val(),
+		"email" : $('#email').val(),
+		"expertise" : $('#expertise').val(),
+		"description" : $('#description').val()
+	});
+}
