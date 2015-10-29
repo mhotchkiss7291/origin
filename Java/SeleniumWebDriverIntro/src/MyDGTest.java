f410a35e4e69090c0d20d53f4d0f0d57d2f40aef
Mark Hotchkiss <mhotchki@DX5VK7YQ1.digitalglobe.com>
2015-10-29 10:43:49 -0600

    Add conif files

--- Java/SeleniumWebDriverIntro/src/MyDGTest.java
+++ Java/SeleniumWebDriverIntro/src/MyDGTest.java
@@ -1,13 +1,14 @@
-import java.awt.AWTException;
-import java.awt.Robot;
-
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.Point;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.interactions.Action;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -15,6 +16,8 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 public class MyDGTest {
 
 	WebDriver driver = null;
+	String username = null;
+	String password = null;
 
 	public static void main(String[] args) {
 
@@ -27,6 +30,8 @@ public class MyDGTest {
 	}
 
 	public void login() {
+
+		loadProperties();
 
 		org.openqa.selenium.Proxy proxy = new org.openqa.selenium.Proxy();
 		proxy.setSslProxy("moz-proxy://gdenwcflgmt.digitalglobe.com" + ":" + 8080);
@@ -63,19 +68,18 @@ public class MyDGTest {
 
 		wait(2);
 
-
-		WebElement drawAOIButton =  driver.findElement(By.xpath("//*[@id='mapDiv']/div[3]/div[1]/div[5]/a/i"));
+		WebElement drawAOIButton = driver.findElement(By.xpath("//*[@id='mapDiv']/div[3]/div[1]/div[5]/a/i"));
 
 		Point buttonLocation = drawAOIButton.getLocation();
 		Point start = new Point(buttonLocation.getX(), buttonLocation.getY());
 		System.out.println("x = " + start.getX() + " , y = " + start.getY());
 
 		Actions actions = new Actions(driver);
-        actions.moveToElement(drawAOIButton).build().perform();
-        actions.moveByOffset( 30, 0).build().perform();;
-        actions.clickAndHold().build().perform();;
-        actions.moveByOffset( 300, 300).build().perform();;
-        actions.release().build().perform();;
+		actions.moveToElement(drawAOIButton).build().perform();
+		actions.moveByOffset(30, 0).build().perform();
+		actions.clickAndHold().build().perform();
+		actions.moveByOffset(300, 300).build().perform();
+		actions.release().build().perform();
 		wait(5);
 	}
 
@@ -89,6 +93,36 @@ public class MyDGTest {
 			Thread.sleep(seconds * 1000);
 		} catch (InterruptedException e) {
 			e.printStackTrace();
+		}
+	}
+
+	public void loadProperties() {
+
+		InputStream inputStream = null;
+
+		try {
+			Properties prop = new Properties();
+			String propFileName = "config.properties";
+
+			inputStream = getClass().getClassLoader().getResourceAsStream(propFileName);
+
+			if (inputStream != null) {
+				prop.load(inputStream);
+			} else {
+				throw new FileNotFoundException("property file '" + propFileName + "' not found in the classpath");
+			}
+
+			this.username = prop.getProperty("username");
+			this.password = prop.getProperty("password");
+			System.out.println("username = " + username + " , password = " + password);
+		} catch (Exception e) {
+			System.out.println("Exception: " + e);
+		} finally {
+			try {
+				inputStream.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		}
 	}
 
