869bb655100ee1fce1794a9d49c2680e1d27f984
Mark Hotchkiss <mhotchki@DX5VK7YQ1.digitalglobe.com>
2015-11-02 17:37:37 -0700

    Clean up Advanced button click

--- Java/SeleniumWebDriverIntro/src/MyDGTest.java
+++ Java/SeleniumWebDriverIntro/src/MyDGTest.java
@@ -25,12 +25,15 @@ public class MyDGTest {
 
 	public static void main(String[] args) {
 
+		String catID = "1030010049849E00";
+		
 		MyDGTest mdgt = new MyDGTest();
 		mdgt.login();
 		mdgt.goToAdvancedSearch();
-		mdgt.drawRectangleAOI();
+		mdgt.drawRectangleAOIOverLongBeach();
 		mdgt.scanSifResultsGrid();
-		mdgt.scanFeatureListForArchive();
+		mdgt.scanAndSelectArchiveFeatureFromList(catID);
+		
 		// mdgt.quit();
 
 	}
@@ -47,131 +50,11 @@ public class MyDGTest {
 		this.driver = new FirefoxDriver(dc);
 		driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
 		driver.get("https://services-test.digitalglobe.com/myDigitalGlobe/login");
-		// driver.manage().window().maximize();
+		driver.manage().window().maximize();
 		driver.findElement(By.xpath("//*[@id='username']")).sendKeys("markh_dibu");
 		driver.findElement(By.xpath("//*[@id='pw']")).sendKeys("test");
 		driver.findElement(By.xpath("//*[@id='acceptTOS']")).click();
 		driver.findElement(By.xpath("//*[@id='loginButton']")).click();
-
-	}
-
-	public void goToAdvancedSearch() {
-
-		driver.findElement(By.xpath("//*[@id='leaflet-control-geosearch-qry']")).sendKeys("Long Beach, CA");
-		driver.findElement(By.xpath("//*[@id='leaflet-control-geosearch-qry']")).sendKeys(Keys.RETURN);
-		driver.findElement(By.xpath("//*[@id='test_myImagery']/span[1]")).click();
-		driver.findElement(By.xpath("//*[@id='test_sifSearch']/span")).click();
-
-	}
-
-	public void drawRectangleAOI() {
-
-		driver.findElement(By.xpath("//*[@id='mapDiv']/div[3]/div[1]/div[5]/a/i")).click();
-		driver.findElement(By.xpath("//*[@id='sif-search-rect']")).click();
-		wait(2);
-		WebElement drawAOIButton = driver.findElement(By.xpath("//*[@id='mapDiv']/div[3]/div[1]/div[5]/a/i"));
-		Actions actions = new Actions(driver);
-		actions.moveToElement(drawAOIButton).build().perform();
-		actions.moveByOffset(30, 0).build().perform();
-		actions.clickAndHold().build().perform();
-		actions.moveByOffset(600, 400).build().perform();
-		actions.release().build().perform();
-		wait(5);
-	}
-
-	public void scanSifResultsGrid() {
-
-		driver.switchTo().defaultContent();
-		WebElement archiveRows = driver.findElement(By.xpath("//*[@id='sifResultsGrid']/table/tbody"));
-
-		// Get List of rows
-		List<WebElement> rows = archiveRows.findElements(By.tagName("tr"));
-		java.util.Iterator<WebElement> i = rows.iterator();
-
-		this.feature_list = rows;
-
-	}
-
-	public void scanFeatureListForArchive() {
-
-		java.util.Iterator<WebElement> i = this.feature_list.iterator();
-
-		while (i.hasNext()) {
-			WebElement row = i.next();
-			System.out.println("data-featureid = " + row.getAttribute("data-featureid"));
-
-			List<WebElement> cells = row.findElements(By.tagName("td"));
-			java.util.Iterator<WebElement> j = cells.iterator();
-
-			while (j.hasNext()) {
-				WebElement cell = j.next();
-				//System.out.println("cell text = " + cell.getText());
-				//System.out.println("clss = " + cell.getAttribute(arg0));
-			}
-		}
-
-		// driver.switchTo().defaultContent();
-		// click on the 5th feature in the list's advFeatureOptionsButton
-		// WebElement archive =
-		// driver.findElement(By.xpath("//*[@id='sifResultsGrid']/table/tbody/tr[5]/td/*[@id='advFeatureOptionsButton']/a/i"));
-
-		// WebElement archiveRows =
-		// driver.findElement(By.xpath("//*[@id='sifResultsGrid']/table/tbody"));
-
-		// Get List of cells
-		// List<WebElement> cells = archiveRows.findElements(By.tagName("tr"));
-		// java.util.Iterator<WebElement> i = rows.iterator();
-
-		// while (i.hasNext()) {
-		// WebElement row = i.next();
-		// System.out.println("text = " + row.getText());
-		// }
-
-		// this.feature_list = rows;
-
-		// archive.click();
-
-		// highlightElement(archive);
-		// wait(30);
-
-		// Scan the feature sifResultsGrid for feature labels
-		// WebElement archiveTable =
-		// driver.findElement(By.xpath("//*[@id='sifResultsGrid']/table/tbody/tr"));
-
-		// List<WebElement> rows = archiveTable.findElements(By.tagName("td"));
-		// java.util.Iterator<WebElement> i = rows.iterator();
-
-		// while (i.hasNext()) {
-		// WebElement row = i.next();
-		// System.out.println("text = " + row.getText());
-		// System.out.println("tag = " + row.getTagName());
-		// System.out.println("class = " + row.getClass());
-		// }
-
-		// 1030010049849E00
-		// /html/body/div[1]/div/div[1]/div[1]/div[2]/div[1]/div[1]/div[3]/table/tbody/tr[7]
-
-		// driver.findElement(By.cssSelector(".dataLabel,.dataLabelWide");
-
-		// if (row.getText().equals("Archive 2015-09-17 WV02")) {
-
-		// System.out.println("i = " + i);
-
-		// wait(30);
-
-		// WebElement desiredFeature =
-		// driver.findElement(By.xpath("//*[@id='advFeatureOptionsButton']/a/i"));
-		// highlightElement(desiredFeature);
-		// List<WebElement> advFeatureOptionsRows =
-		// archiveTable.findElements(By.tagName("li"));
-		// java.util.Iterator<WebElement> j =
-		// advFeatureOptionsRows.iterator();
-		// while (j.hasNext()) {
-		// WebElement advFeatureOptionsRow = j.next();
-		// System.out.println(advFeatureOptionsRow.getText());
-		// }
-		// }
-		// }
 
 	}
 
@@ -227,5 +110,63 @@ public class MyDGTest {
 		}
 		js.executeScript("arguments[0].setAttribute('style', '" + originalStyle + "');", element);
 	}
+
+	public void goToAdvancedSearch() {
+		driver.findElement(By.xpath("//*[@id='leaflet-control-geosearch-qry']")).sendKeys("Long Beach, CA");
+		driver.findElement(By.xpath("//*[@id='leaflet-control-geosearch-qry']")).sendKeys(Keys.RETURN);
+		driver.findElement(By.xpath("//*[@id='test_myImagery']/span[1]")).click();
+		driver.findElement(By.xpath("//*[@id='test_sifSearch']/span")).click();
+	}
+
+	public void drawRectangleAOIOverLongBeach() {
+		driver.findElement(By.xpath("//*[@id='mapDiv']/div[3]/div[1]/div[5]/a/i")).click();
+		driver.findElement(By.xpath("//*[@id='sif-search-rect']")).click();
+		wait(2);
+		WebElement drawAOIButton = driver.findElement(By.xpath("//*[@id='mapDiv']/div[3]/div[1]/div[5]/a/i"));
+		Actions actions = new Actions(driver);
+		actions.moveToElement(drawAOIButton).build().perform();
+		actions.moveByOffset(30, 0).build().perform();
+		actions.clickAndHold().build().perform();
+		actions.moveByOffset(600, 400).build().perform();
+		actions.release().build().perform();
+		wait(5);
+	}
+
+	public void scanSifResultsGrid() {
+
+		driver.switchTo().defaultContent();
+		WebElement archiveRows = driver.findElement(By.xpath("//*[@id='sifResultsGrid']/table/tbody"));
+
+		// Get List of rows
+		List<WebElement> rows = archiveRows.findElements(By.tagName("tr"));
+		java.util.Iterator<WebElement> i = rows.iterator();
+
+		this.feature_list = rows;
+
+	}
+
+	public void scanAndSelectArchiveFeatureFromList(String catID) {
+
+		// Scan feature list for a particular archive CatID
+		java.util.Iterator<WebElement> element = this.feature_list.iterator();
+
+		// Count the rows to find the CatID arg and click the advFeaturOptionsButton
+		int i = 0;
+		while (element.hasNext()) {
+			WebElement row = element.next();
+			i++;
+			// System.out.println("data-featureid =
+			// "row.getAttribute("data-featureid"));
+
+			if (row.getAttribute("data-featureid").equals(catID)) {
+				driver.switchTo().defaultContent();
+				System.out.println("i = " + i);
+				WebElement button = driver.findElement(By.xpath(
+						"//*[@id='sifResultsGrid']/table/tbody/tr[" + i + "]/td/*[@id='advFeatureOptionsButton']/a/i"));
+				button.click();
+			}
+		}
+	}
+
 
 }
