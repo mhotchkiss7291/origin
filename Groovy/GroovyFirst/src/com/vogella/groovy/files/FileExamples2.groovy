325ead80b86503e8cc2d4f6d6a268eca5a3f0f1b
Mark Hotchkiss <mhotchkiss@server.local>
2015-11-08 13:11:56 -0700

    Add groovy file examples

---
+++ Groovy/GroovyFirst/src/com/vogella/groovy/files/FileExamples2.groovy
@@ -0,0 +1,11 @@
+package com.vogella.groovy.files
+
+// write the content of the file to the console
+File file = new File("output.txt")
+file.write "Hello\n"
+file.append "Testing\n"
+file << "More appending...\n"
+File result = new File("output.txt")
+println (result.text)
+// clean-up
+file.delete()
