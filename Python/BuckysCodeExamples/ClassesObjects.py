337449690e3d9b7d6721b60a13c4c052247f7c69
Mark Hotchkiss <mhotchki@DX5VK7YQ1.digitalglobe.com>
2016-01-05 14:41:14 -0700

    Add classes/objects

---
+++ Python/BuckysCodeExamples/ClassesObjects.py
@@ -0,0 +1,22 @@
+class Enemy:
+    life = 3
+
+    def attack(self):
+        print('ouch!')
+        self.life -= 1
+
+    def checkLife(self):
+        if self.life <= 0:
+            print('I am dead')
+        else:
+            print(str(self.life) + " life left")
+
+# Construct objects
+enemy1 = Enemy()
+enemy2 = Enemy()
+
+# Exercise functions
+enemy1.attack()
+enemy1.attack()
+enemy1.checkLife()
+enemy2.checkLife()
