7143bb2a4ac9d9f8bf663f78d417cae4a24296a5
mhotchkiss <mark.hotchkiss@gmail.com>
2016-01-07 18:15:03 -0700

    Failed attempt

---
+++ Python/BuckysCodeExamples/ImageCombined.py
@@ -0,0 +1,12 @@
+from PIL import Image
+
+# Identify two images
+boat = Image.open("TestImage-1.jpg")
+salt_pier = Image.open("TestImage-2.jpg")
+area = (100, 100, 300, 300)
+
+#salt_pier.paste(salt_pier, area)
+salt_pier.Image.
+# Why doesn't PyCharm find this class???
+
+salt_pier.show()
