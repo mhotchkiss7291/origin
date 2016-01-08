f8754ef9e27c33af95e3da114fd84873230110eb
mhotchkiss <mark.hotchkiss@gmail.com>
2016-01-08 15:24:26 -0700

    Paste file working

--- Python/BuckysCodeExamples/ImageCombined.py
+++ Python/BuckysCodeExamples/ImageCombined.py
@@ -1,12 +1,18 @@
 from PIL import Image
 
 # Identify two images
-boat = Image.open("TestImage-1.jpg")
-salt_pier = Image.open("TestImage-2.jpg")
-area = (100, 100, 300, 300)
+boat = Image.open("CruiseShipFull.jpg")
+salt_pier = Image.open("SaltPierReduced.jpg")
 
-#salt_pier.paste(salt_pier, area)
-salt_pier.Image.
-# Why doesn't PyCharm find this class???
+# Where to place the image? 20 pixels inside the
+# top left corner of the base layer image
+upperLeftAnchor = (20, 20)
 
+boat.show()
 salt_pier.show()
+
+# Paste one image on another
+boat.paste(salt_pier, upperLeftAnchor)
+boat.show()
+
+
