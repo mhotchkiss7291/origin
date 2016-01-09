c9193d56a759e5c42dcbbfb4f090e7a0c4183068
mhotchkiss <mark.hotchkiss@gmail.com>
2016-01-09 13:49:31 -0700

    Working Image.merge example

--- Python/BuckysCodeExamples/ColorChannelsMerge.py
+++ Python/BuckysCodeExamples/ColorChannelsMerge.py
@@ -1,15 +1,31 @@
 from PIL import Image
 
-ship = Image.open("CruiseShip.jpg")
-salt_pier = Image.open("SaltPierReduced.jpg")
+ship = Image.open("CruiseShip600x400.jpg")
+ship.show()
+
+turtle = Image.open("Turtle600x400.jpg")
+turtle.show()
 
 # Split the RGB channel data
 r1, g1, b1 = ship.split()
-r2, g2, b2 = salt_pier.split()
+print(r1, g1, b1)
+r2, g2, b2 = turtle.split()
+print(r2, g2, b2)
 
 #print(r1)
 
 # I don't know why this does not work
-new_img = Image.merge("RGB", (r1, g2, b1) )
+scramble_channel_ship = Image.merge('RGB', (g1, b1, r1) )
+scramble_channel_ship.show()
+
+scramble_channel_turtle = Image.merge('RGB', (g2, b2, r2) )
+scramble_channel_turtle.show()
+
+scrambled_images1 = Image.merge('RGB', (r2, g1, b2) )
+scrambled_images1.show()
+
+scrambled_images2 = Image.merge('RGB', (r1, g2, b1) )
+scrambled_images2.show()
+
 
 
