5025f7460b46136e9491096ef0e485e0720ffb71
mhotchkiss <mark.hotchkiss@gmail.com>
2016-01-09 14:16:29 -0700

    Fixed image example

--- Python/BuckysCodeExamples/CroppingImages.py
+++ Python/BuckysCodeExamples/CroppingImages.py
@@ -1,6 +1,6 @@
 from PIL import Image
 
-img = Image.open("TestImage-4.jpg")
+img = Image.open("CruiseShip.jpg")
 
 # a 4 item tuple
 area = (200, 200, 600, 775)
@@ -8,5 +8,8 @@ area = (200, 200, 600, 775)
 # Crop the area
 cropped_image = img.crop(area)
 
-img.show()
+# On my Mac, the big image comes up
+# before the cropped image does,
+# consistently using Preview
 cropped_image.show()
+img.show()
