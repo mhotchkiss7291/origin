0e89033a36f1eb14eeeebf88f90c7f3b79db796d
Mark Hotchkiss <mhotchkiss@rslmac19182.rosettastone.local>
2016-01-22 12:45:33 -0700

    Add working RestAssured project

---
+++ Java/RestAssured/src/main/java/RestTest.java
@@ -0,0 +1,42 @@
+import static com.jayway.restassured.RestAssured.get;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.jayway.restassured.response.Response;
+
+
+public class RestTest {
+
+    @Test
+    public void getRequestFindCapital() throws JSONException {
+
+        //make get request to fetch capital of norway
+        Response resp = get("http://restcountries.eu/rest/v1/name/norway");
+
+        //Fetching response in JSON
+        JSONArray jsonResponse = new JSONArray(resp.asString());
+
+        //Fetching value of capital parameter
+        String capital = jsonResponse.getJSONObject(0).getString("capital");
+
+        // Print "Oslo"`
+        System.out.println(capital);
+
+        //Asserting that capital of Norway is Oslo
+        Assert.assertEquals(capital, "Oslo");
+    }
+
+    public static void main(String args[]) {
+
+        RestTest rt = new RestTest();
+
+        try {
+            rt.getRequestFindCapital();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+}
