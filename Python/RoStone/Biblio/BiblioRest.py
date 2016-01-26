a5b128b314a9c378e095d0f90bd567c286b97ab1
Mark Hotchkiss <mhotchkiss@rslmac19182.rosettastone.local>
2016-01-26 10:14:18 -0700

    Working biblio request with generated tokens

--- Python/RoStone/Biblio/BiblioRest.py
+++ Python/RoStone/Biblio/BiblioRest.py
@@ -1,40 +1,30 @@
 import requests
 import json
+import PythonTokenGenerator
 
 # cURL command to be converted
-#curl -X GET -H "Accept: application/json" -H "Authorization: Bearer $auth_token" $bib_host/content/user_activity_set_summary/$user_id
+# curl -X GET -H "Accept: application/json" -H "Authorization: Bearer $auth_token" $bib_host/content/user_activity_set_summary/$user_id
 
 # Test environment URL
-url="https://biblio-qa2.dev.rosettastone.com"
+url = "https://biblio-qa2.dev.rosettastone.com"
 
-# Service end point
-end_point="/content/user_activity_set_summary/"
+# Set service end point, user, environment
+end_point = "/content/user_activity_set_summary/"
+user = "mhotchkiss@rosettastone.com"
+environment = "qa2"
 
-# User credentials
-# Need a program to generate this from the
-# login like idenifier: mhotchkiss@rosettastone.com
-user_id="8c520215-1e36-4bc4-a33b-009ddcc7248a"
-
-# This expires every few hours and needs to be renewed
-# Need to add the http://cloudan-toolkit.trstone.com/tokenizer/#
-# utility so that expired tokens will be renewed
-auth_token="904b715a-c77e-48df-89f9-1131fe345fc7"
-
+# Get the generated userId and auth_token
+response = PythonTokenGenerator.get_token(environment, user)
+user_id = response[0]
+auth_token = response[1]
 
 headers = {
     'Accept': 'application/json',
     'Authorization': 'Bearer ' + auth_token
 }
 
-# This script converts cURL commands to python by constructing the command by variables:
-# url
-# endpoint
-# auth_token
-
-r = requests.get(url + end_point + user_id , headers=headers)
+r = requests.get(url + end_point + user_id, headers=headers)
 results = json.loads(r.text)
 print(json.dumps(results))
-
-
 
 
--- Python/RoStone/Biblio/PythonTokenGenerator.py
+++ Python/RoStone/Biblio/PythonTokenGenerator.py
@@ -1,30 +1,35 @@
-def get_token(env='qa1'):
-    if env=="qa":
-        env="qa1"
+import json
+import requests
+from urllib.parse import urlencode
 
-    client_id='siren'
-    grant='password'
-    user_id='soa-only@rosettastone.com'
-    user_pw='password'
+
+def get_token(env, user_id):
+    #if env == "qa":
+    #    env = "qa2"
+
+    client_id = 'siren'
+    grant = 'password'
+    #user_id = 'mhotchkiss@rosettastone.com'
+    user_pw = 'password'
     if env == "prod" or env == "prod-wdc":
         user_id = "dadams+soa_production@rosettastone.com"
-        password= "password"
-    refresh_token=None
+        password = "password"
+    refresh_token = None
 
     data = {}
-    header_dict={}
-
+    header_dict = {}
 
     header_dict['Content-Type'] = 'application/x-www-form-urlencoded;application/json'
 
     data['grant_type'] = grant
     data['client_id'] = client_id
     endpoint = ""
-    if "prod" in env.lower():#1621d227-1105-4939-ba4d-d2d53c5cd5eb
+    if "prod" in env.lower():  # 1621d227-1105-4939-ba4d-d2d53c5cd5eb
         endpoint = "https://tully.rosettastone.com/oauth/token"
     else:
         endpoint = "https://tully-{}.dev.rosettastone.com/oauth/token".format(env)
-    print endpoint
+    print
+    endpoint
 
     header_dict['Host'] = "tully-{}.dev.rosettastone.com".format(env)
     header_dict['X-Livemocha-Product'] = 'Product:Aria'
@@ -37,9 +42,13 @@ def get_token(env='qa1'):
         data['password'] = user_pw
 
     payload = urlencode(data)
-    response =  requests.post(endpoint, data=payload, headers=header_dict)
+    response = requests.post(endpoint, data=payload, headers=header_dict)
 
     response_string = json.loads(response.text)
+    #print(response_string)
 
+    userId = response_string.get("userId")
     token = response_string.get("access_token")
-    return token
+
+    return (userId, token)
+
