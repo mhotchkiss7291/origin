c9e89a0166ae4dff64dbfa2871eb5624f4f4cdde
mhotchkiss <mark.hotchkiss@gmail.com>
2016-02-06 14:21:15 -0700

    Refine Biblio Request test harness

--- Python/RoStone/Biblio/BiblioRest.py
+++ Python/RoStone/Biblio/BiblioRest.py
@@ -6,12 +6,12 @@ import PythonTokenGenerator
 # curl -X GET -H "Accept: application/json" -H "Authorization: Bearer $auth_token" $bib_host/content/user_activity_set_summary/$user_id
 
 # Test environment URL
-url = "https://biblio-qa2.dev.rosettastone.com"
+url = "https://biblio-qa1.dev.rosettastone.com"
 
 # Set service end point, user, environment
 end_point = "/content/user_activity_set_summary/"
-user = "mhotchkiss@rosettastone.com"
-environment = "qa2"
+user = "mhotchkiss-qa1@rosettastone.com"
+environment = "qa1"
 
 # Get the generated userId and auth_token
 response = PythonTokenGenerator.get_token(environment, user)
@@ -23,8 +23,12 @@ headers = {
     'Authorization': 'Bearer ' + auth_token
 }
 
+# Make the request
 r = requests.get(url + end_point + user_id, headers=headers)
+
+# Process and format the response
 results = json.loads(r.text)
-print(json.dumps(results))
+pretty = json.dumps(results, indent=2)
+print(pretty)
 
 
