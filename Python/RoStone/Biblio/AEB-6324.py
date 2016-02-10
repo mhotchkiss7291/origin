db3861c76ad2774153df2e36bbb5dcf8fa5b0fd9
Mark Hotchkiss <mhotchkiss@rslmac19182.rosettastone.local>
2016-02-10 12:32:49 -0700

    Edit request so it works

--- Python/RoStone/Biblio/AEB-6324.py
+++ Python/RoStone/Biblio/AEB-6324.py
@@ -1,73 +1,45 @@
+from urllib import urlencode
+
 import requests
 import json
 from PythonTokenGenerator import get_token
 
-# Test variables that need to be arguments
-url = "https://biblio-qa1.dev.rosettastone.com"
-endpoint = url + "/schedule/join/attendee?meetingID=testmeeting&host=rosettastoneqadev.adobeconnect.com"
-user = "mhotchkiss@rosettastone.com"
-environment = "qa1"
+#curl \
+#'https://scholar-qa2.dev.rosettastone.com/schedule/join/attendee
+# ?meetingID=aeb-6324-qa2&
+# host=rosettastoneqadev.adobeconnect.com
+# &access_token=f60e2d38-d43a-4d45-93f7-67de408b5758' \
+#--include
+
+url = "https://biblio-qa2.dev.rosettastone.com"
+endpoint = url + "/schedule/join/attendee"
+meetingID = 'aeb-6324-qa2'
+host = 'rosettastoneqadev.adobeconnect.com'
+user = "mhotchkiss-qa2@rosettastone.com"
+environment = "qa2"
+
+print(endpoint)
 
 # Get auth_token from user
 response = get_token(environment, user)
 user_id = response[0]
-auth_token = response[1]
+#auth_token = response[1]
+auth_token = 'f60e2d38-d43a-4d45-93f7-67de408b5758'
+print(auth_token)
 
 # Start Dan code
 data = {}
-#data['grant_type'] = grant
-#data['client_id'] = client_id
+data['meetingID'] = meetingID
+data['host'] = host
+data['access_token'] = auth_token
 
-#Header setting
+# Header setting
 header_dict = {}
-header_dict['Authorization: Bearer'] = auth_token
-header_dict['X-Rosettastone-Product-Id'] = "product.82692f39-a40e-4051-a6dc-e0cbf027c80b"
-header_dict['X-Livemocha-Product:'] = "Product:Aria"
-
-
-response = requests.get(endpoint, headers=header_dict)
-#response = requests.get(endpoint + user_id, headers=headers)
 
 payload = urlencode(data)
-#
-##rest_response = requests.get(url + end_point + user_id, headers=headers)
-##results = json.loads(rest_response.text)
-##print(json.dumps(results))
-#
-#
-# client_id = 'siren'
-#    grant = 'password'
-#    #user_id = 'mhotchkiss@rosettastone.com'
-#    user_pw = 'password'
-#    if env == "yrod" or env == "prod-wdc":
-#        user_id = "dadams+soa_production@rosettastone.com"
-#        password = "password"
-#    refresh_token = None
-#
-#    endpoint = ""
-#    if "prod" in env.lower():  # 1621d227-1105-4939-ba4d-d2d53c5cd5eb
-#        endpoint = "https://tully.rosettastone.com/oauth/token"
-#    else:
-#        endpoint = "https://tully-{}.dev.rosettastone.com/oauth/token".format(env)
-#    print
-#    endpoint
-#
-#    header_dict['Host'] = "tully-{}.dev.rosettastone.com".format(env)
-#    header_dict['X-Livemocha-Product'] = 'Product:Aria'
-#    header_dict['Referer'] = 'https://' + 'livemocha_client'
-#
-#    if refresh_token:
-#        data['refresh_token'] = refresh_token
-#    else:
-#        data['username'] = user_id
-#        data['password'] = user_pw
-#
-#    payload = urlencode(data)
-#
-#    response_string = json.loads(response.text)
-#    #print(response_string)
-#
-#    userId = response_string.get("userId")
-#    token = response_string.get("access_token")
-#
-#    return (userId, token)
+
+response = requests.get(endpoint, data=payload, headers=header_dict)
+
+results = json.loads(response.text)
+pretty = json.dumps(results, indent=2)
+print(pretty)
