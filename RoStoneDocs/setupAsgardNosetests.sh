a566b0cdf65c6fa6354615a570c5ef007c384124
Mark Hotchkiss <mhotchkiss@rslmac19182.rosettastone.local>
2016-02-09 18:46:12 -0700

    Add TEST_ENV echo

--- RoStoneDocs/setupAsgardNosetests.sh
+++ RoStoneDocs/setupAsgardNosetests.sh
@@ -4,8 +4,3 @@ echo "Git Done.."
 cp app.cfg ./asgard/util/src/python/config
 echo "app.cfg copied"
 pip install -r ./asgard/util/src/python/config/requirements.txt
-echo "requirements.txt installed"
-cd ./asgard/util/src/python
-echo "Move to test dir, running tests.."
-# All output to file
-nosetests -v ./test/biblio &> ~/TestReport.out
