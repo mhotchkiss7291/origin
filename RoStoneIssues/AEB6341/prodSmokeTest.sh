1f96a9cb938c6ad5694b509bac9924d82cb31574
Mark Hotchkiss <mhotchkiss@rslmac19182.rosettastone.local>
2016-02-10 11:30:52 -0700

    Add smoke test

---
+++ RoStoneIssues/AEB6341/prodSmokeTest.sh
@@ -0,0 +1,4 @@
+curl \
+https://prism.rosettastone.com/reports/usage/product/Product%3AAria/user/bf9ffa3d-e79a-425e-a8d7-656f183fd5f7 \
+-H 'Authorization: Bearer d1037fee-893b-45f0-9ba5-8de847106803' \
+| python -mjson.tool
