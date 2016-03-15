4bd5fde206502625f58444f99a1cc8784a3c34b7
mhotchkiss <mark.hotchkiss@gmail.com>
2016-03-15 17:08:15 -0600

    Fix Python

--- Work/ProductCourseSummaryIncludeVersion.py
+++ Work/ProductCourseSummaryIncludeVersion.py
@@ -5,23 +5,26 @@ import json
 import PythonTokenGenerator
 
 # curl \
-# 'https://biblio-qa2.dev.rosettastone.com/content/productCourseSummary/Product:Aria/en-US?includeVersion=true' \
+# 'https://biblio-qa1.dev.rosettastone.com/content/productCourseSummary/Product:Aria/en-US?includeVersion=true' \
 # -H 'X-Livemocha-Product: Product:Aria' \
 # -H 'Content-Type: application/json;charset=UTF-8' \
-# -H 'Authorization: Bearer 43f0ffeb-1592-4060-a2c2-cf2ad5183d33' \
-# -H 'Origin: https://aebeditor-qa2.dev.rosettastone.com' \
+# -H 'Authorization: Bearer 4fee69f7-0862-4cca-8fe5-4f8e1a5971df' \
+# -H 'Origin: https://aebeditor-qa1.dev.rosettastone.com' \
 # -H 'Accept-Encoding: gzip, deflate, sdch' \
 # -H 'Accept-Language: en-US,en;q=0.8' \
 # -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.116 Safari/537.36' \
 # -H 'Accept: application/json, text/plain, */*' \
-# -H 'Referer: https://aebeditor-qa2.dev.rosettastone.com/' \
+# -H 'Referer: https://aebeditor-qa1.dev.rosettastone.com/' \
 # -H 'X-Requested-With: Ceres' \
 # -H 'Connection: keep-alive' \
-# --compressed
+# --compressed \
+#  | python -mjson.tool
+
 
 # Strings
 url = "https://biblio-qa1.dev.rosettastone.com"
 endpoint = "/content/productCourseSummary/Product:Aria/en-US?includeVersion=true"
+
 
 # For auth_token
 user = "mhotchkiss-qa1@rosettastone.com"
@@ -46,7 +49,7 @@ header_data = {
     'Connection': 'keep-alive'
 }
 
-response = requests.get(url + endpoint + user_id, headers=header_data)
+response = requests.get(url + endpoint, headers=header_data)
 
 # Process and format the response
 results = json.loads(response.text)
