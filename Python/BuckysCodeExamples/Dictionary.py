fea0a319a620a2a77eaa983e406b5d7caa449458
mhotchkiss <mark.hotchkiss@gmail.com>
2016-03-25 15:53:49 -0600

    Clarification

--- Python/BuckysCodeExamples/Dictionary.py
+++ Python/BuckysCodeExamples/Dictionary.py
@@ -1,7 +1,16 @@
-classmates = {'Tony': ' cool but smells', 'Emma': ' sits behind me', 'Lucy': ' asks too many questions'}
+classmates = {
+    'Tony': ' cool but smells',
+    'Emma': ' sits behind me',
+    'Lucy': ' asks too many questions'
+}
 
-#print(classmates)
-#print(classmates['Emma'])
+# print(classmates)
+# print(classmates['Emma'])
 
-for k,v in classmates.items():
-    print("Key = "+  k + ": Value: " +v)
+for k, v in classmates.items():
+    print("Key = " + k + ": Value: " + v)
+
+# Output
+# Key = Tony: Value:  cool but smells
+# Key = Emma: Value:  sits behind me
+# Key = Lucy: Value:  asks too many questions
