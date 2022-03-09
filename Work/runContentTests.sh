5b2018dd2d214b5d8fb868195bb9c6cd3f8b2b18
mhotchkiss <mhotchkiss@rslmac16017.local>
2022-03-09 09:49:43 -0700

    Clean up

--- Work/runContentTests.sh
+++
@@ -1,3 +0,0 @@
-#Tests the endpoint /content/user_activity_set_summary/<userId> returns ... FAIL
-#Tests the endpoint /content/user_activity_set_summary/<userId>?course=<courseId> returns ... FAIL
-nosetests -v -a 'acceptance','mrh' --with-xunit test/biblio/content_test.py:mrh_stuff
