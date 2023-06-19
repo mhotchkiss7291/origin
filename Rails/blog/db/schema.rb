9d1f7d5f80304d78680d0656416dfff016b6bebd
Mark Hotchkiss <mhotchkiss@Clark.local>
2023-06-19 14:43:32 -0600

    stuff

---
+++ Rails/blog/db/schema.rb
@@ -0,0 +1,10 @@
+
+ActiveRecord::Schema[7.0].define(version: 2023_06_19_195903) do
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+end
