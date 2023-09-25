1fd8ebdc2b6218c002324b5697a3e8920a07f600
Mark Hotchkiss <mhotchkiss@Clark.local>
2023-09-25 15:40:15 -0600

    Update

--- Cypress/cypress/e2e/mrh.cy.js
+++ Cypress/cypress/e2e/mrh.cy.js
@@ -4,6 +4,6 @@
 
 		cy.visit('https://google.com')
 		cy.get('#APjFqb').type('wsj{enter}')
-		cy.get(':nth-child(5) > .cIkxbf > .usJj9c > h3 > .l').click
+		//cy.get(':nth-child(5) > .cIkxbf > .usJj9c > h3 > .l').click()
 
 	})
