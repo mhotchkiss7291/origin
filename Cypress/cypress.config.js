7b70df63f43e2d3f12cb5a907c26560f42efb2b5
Mark Hotchkiss <mhotchkiss@Clark.local>
2023-09-26 11:20:03 -0600

    Progress

--- Cypress/cypress.config.js
+++ Cypress/cypress.config.js
@@ -1,9 +1,14 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
+
+  defaultCommandTimeout: 6000,
+  watchForFileChanges: true,
+
 });
--- Cypress/cypress/e2e/mrh.cy.js
+++ Cypress/cypress/e2e/mrh.cy.js
@@ -4,6 +4,8 @@
 
 		cy.visit('https://google.com')
 		cy.get('#APjFqb').type('wsj{enter}')
+		//cy.get('#APjFqb', {timeout: 6000}).type('wsj{enter}')
 		//cy.get(':nth-child(5) > .cIkxbf > .usJj9c > h3 > .l').click()
+		cy.get(':nth-child(4) > .cIkxbf > .usJj9c > h3 > .l').click()
 
 	})
