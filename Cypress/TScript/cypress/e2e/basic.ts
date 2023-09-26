e84755feb0dae1659cdbe708abc90d3b20d0701a
Mark Hotchkiss <mhotchkiss@Clark.local>
2023-09-26 13:45:06 -0600

    Delete

--- Cypress/TScript/cypress/e2e/basic.ts
+++
@@ -1,9 +0,0 @@
-it('google test', function() {
-
-    cy.visit('https://google.com')
-    cy.get('[id="input').type('wsj{enter}')
-    //cy.get(':nth-child(4) > .cIkxbf > .usJj9c > h3 > .l').click()
-    //cy.wait(4000)
-    //cy.contains('Opinion').click()
-
-})
