5058acbb84a8fa8ac8c2388d8fcb5054b9d35c60
Mark Hotchkiss <mhotchkiss@Clark.local>
2023-09-29 16:02:22 -0600

    Stuff

--- Cypress/cypress/e2e/async_spec.cy.js
+++ Cypress/cypress/e2e/async_spec.cy.js
@@ -7,10 +7,23 @@ context('Asyncronous', () => {
     it('it types into an email field', () => {
         cy.visit('/commands/actions')
         cy.findByPlaceholderText('Email').type('test.email.com')
-        cy.wait(5000)
+        // Debugging - no extraction of data
         console.log('test is finished')
+        cy.log('1')
+        cy.log('2')
+        cy.log('3')
     })
 
+    it('it waits for 5 seconds before saying test is finished', () => {
+        cy.visit('/commands/actions')
+        cy.findByPlaceholderText('Email').type('test.email.com')
+        // Use the then() command to hijack cypress asynchronously
+        //cy.wait(5000).then(() => {
+            console.log('then command test is finished')
+        })
+        // Debugging - no extraction of data
+        console.log('test is finished')
+    })
 
     
 })
