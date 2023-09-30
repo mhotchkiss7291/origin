33ba692c0a50857269d4aa968b39e19cc412b4d9
Mark Hotchkiss <mhotchkiss@Clark.local>
2023-09-30 14:14:43 -0600

    Async stuff

--- Cypress/cypress/e2e/async_spec.cy.js
+++ Cypress/cypress/e2e/async_spec.cy.js
@@ -15,11 +15,22 @@ context('Asyncronous', () => {
     })
 
     it('it waits for 5 seconds before saying test is finished', () => {
+
         cy.visit('/commands/actions')
         cy.findByPlaceholderText('Email').type('test.email.com')
+
         // Use the then() command to hijack cypress asynchronously
-        //cy.wait(5000).then(() => {
+        cy.wait(2000).then(() => {
+
+            // What is this?
+            // eslint-disable-next-line no-console
             console.log('then command test is finished')
+            fetch('https://api.spacexdata.com/v3/missions')
+            .then((res) => res.json())
+            .then((data) => {
+                console.log(data)
+            })
+
         })
         // Debugging - no extraction of data
         console.log('test is finished')
