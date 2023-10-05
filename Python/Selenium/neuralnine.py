0f3eb4f549fc6808d5ada79634795ccea9b1ceb0
Mark Hotchkiss <mhotchkiss@Clark.local>
2023-10-05 16:13:15 -0600

    New trials

--- Python/Selenium/neuralnine.py
+++ Python/Selenium/neuralnine.py
@@ -2,7 +2,6 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
-import time
 
 options = Options()
 options.add_experimental_option("detach", True)
@@ -10,8 +9,10 @@ options.add_experimental_option("detach", True)
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),
                           options=options)
 
-driver.get("https://google.com/")
+driver.get("https://neuralnine.com/")
+driver.maximize_window()
 
-time.sleep(2)
-driver.close()
-driver.quit()
+# Get all of the links with an innerHTML type
+links = driver.fine_elements("xpath", "//a[@href]")
+for link in links:
+    print(link)
--- Python/Selenium/orange.py
+++ Python/Selenium/orange.py
@@ -1,8 +1,12 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
+
 import time
+
+#
 
 options = Options()
 options.add_experimental_option("detach", True)
@@ -10,7 +14,10 @@ options.add_experimental_option("detach", True)
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),
                           options=options)
 
-driver.get("https://google.com/")
+driver.get("https://opensource-demo.orangehrmlive.com/")
+
+# Doesn't work yet
+#driver.find_element(By.NAME, 'username').send_keys('Admin')
 
 time.sleep(2)
 driver.close()
